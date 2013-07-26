ffc5233f6bed56fa087202e26ae331660ba60ee3
Aaron Weiss <aaronweiss74@gmail.com>
2013-07-26 01:21:20 -0400

    Updated DataSource to enforce desired features.

--- om-datalib/src/main/java/openmaple/data/DataSource.java
+++ om-datalib/src/main/java/openmaple/data/DataSource.java
@@ -1,13 +1,26 @@
 package openmaple.data;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * An interface describing a basic data source for {@code om-datalib}.
  *
  * @author Aaron Weiss
- * @version 1.0.0
+ * @version 2.0.0
  * @since 7/25/13
  */
-public interface DataSource {
+public abstract class DataSource {
+	protected Map<String, DataEntry> dataEntries;
+	private Lock entriesLock;
+
+	public DataSource() {
+		entriesLock = new ReentrantLock();
+		dataEntries = new HashMap<>();
+	}
+
 	/**
 	 * Gets the name of the data source.
 	 * This can be anything as long as it describes the source uniquely.
@@ -15,14 +28,14 @@ public interface DataSource {
 	 *
 	 * @return the name of the source
 	 */
-	public String getName();
+	public abstract String getName();
 
 	/**
 	 * Gets the root data entry from this data source.
 	 *
 	 * @return the root data entry
 	 */
-	public DataEntry getRoot();
+	public abstract DataEntry getRoot();
 
 	/**
 	 * Looks up a data entry in the source by full path.
@@ -31,8 +44,12 @@ public interface DataSource {
 	 * @return the entry
 	 * @throws NoSuchDataEntryException if entry cannot be found
 	 */
-	public DataEntry lookUp(String path);
-
+	public DataEntry lookUp(String path) {
+		DataEntry ret = tryLookUp(path);
+		if (ret == null)
+			throw new NoSuchDataEntryException(this, path);
+		return ret;
+	}
 
 	/**
 	 * Tries to look up a data entry by full path, returning null if the entry is not found.
@@ -40,5 +57,37 @@ public interface DataSource {
 	 * @param path the path to the entry
 	 * @return the entry or null if it cannot be found
 	 */
-	public DataEntry tryLookUp(String path);
+	public DataEntry tryLookUp(String path) {
+		if (entriesLock.tryLock() && !dataEntries.isEmpty()) {
+			return dataEntries.get(path);
+		} else {
+			return internalLookUp(path);
+		}
+	}
+
+	/**
+	 * Populates an internal {@code Map} with data entries for full-path lookup.
+	 * This should result in a significant speed-up in accessing data entries by full path.
+	 */
+	public void populateEntryMap() {
+		entriesLock.lock();
+		try {
+			internalPopulateEntryMap();
+		} finally {
+			entriesLock.unlock();
+		}
+	}
+
+	/**
+	 * Performs an internal population of the data entry {@code Map} to speed-up full-path lookups.
+	 */
+	protected abstract void internalPopulateEntryMap();
+
+	/**
+	 * Performs an internal data entry lookup. This is to be specified by the particular implementation.
+	 *
+	 * @param path the path to the entry
+	 * @return the entry or null if it cannot be found
+	 */
+	protected abstract DataEntry internalLookUp(String path);
 }
