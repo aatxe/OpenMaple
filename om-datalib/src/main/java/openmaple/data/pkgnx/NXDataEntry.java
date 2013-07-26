ccc8e92eb3231c7cc218dc89d600c9497a48a44b
Aaron Weiss <aaronweiss74@gmail.com>
2013-07-26 03:55:24 -0400

    Added an data implementation for pkgnx.

--- om-datalib/src/main/java/openmaple/data/pkgnx/NXDataEntry.java
+++ om-datalib/src/main/java/openmaple/data/pkgnx/NXDataEntry.java
@@ -1,9 +1,76 @@
 package openmaple.data.pkgnx;
 
+import openmaple.data.DataEntry;
+import openmaple.data.DataEntryType;
+import us.aaronweiss.pkgnx.format.NXNode;
+import us.aaronweiss.pkgnx.format.nodes.*;
+
 /**
- * @author Aaron
- * @version 1.0
+ * An entry of data representing an {@code NXNode} within an {@code NXFile}.
+ *
+ * @author Aaron Weiss
+ * @version 1.0.0
  * @since 7/26/13
  */
-public class NXDataEntry {
+public class NXDataEntry implements DataEntry {
+	private final DataEntryType type;
+	private final NXNode node;
+	private final String path;
+	private DataEntry[] children;
+
+	public NXDataEntry(NXNode node, String path) {
+		this.node = node;
+		this.path = path;
+		children = null;
+
+		if (node instanceof NXNullNode) {
+			type = DataEntryType.None;
+		} else if (node instanceof NXLongNode) {
+			type = DataEntryType.Int64;
+		} else if (node instanceof NXDoubleNode) {
+			type = DataEntryType.Double;
+		} else if (node instanceof NXStringNode) {
+			type = DataEntryType.String;
+		} else if (node instanceof NXPointNode) {
+			type = DataEntryType.Vector;
+		} else if (node instanceof NXBitmapNode) {
+			type = DataEntryType.Bitmap;
+		} else if (node instanceof NXAudioNode) {
+			type = DataEntryType.Audio;
+		} else {
+			type = DataEntryType.Unknown;
+		}
+	}
+
+	@Override
+	public String getName() {
+		return node.getName();
+	}
+
+	@Override
+	public String getPath() {
+		return path;
+	}
+
+	@Override
+	public Object getData() {
+		return node.get();
+	}
+
+	@Override
+	public DataEntryType getType() {
+		return type;
+	}
+
+	@Override
+	public DataEntry[] getChildEntries() {
+		if (children == null) {
+			children = new DataEntry[node.getChildCount()];
+			int index = 0;
+			for (NXNode child : node) {
+				children[index++] = new NXDataEntry(child, path + "/" + child.getName());
+			}
+		}
+		return children;
+	}
 }
--- om-datalib/src/main/java/openmaple/data/pkgnx/NXDataSource.java
+++ om-datalib/src/main/java/openmaple/data/pkgnx/NXDataSource.java
@@ -1,9 +1,88 @@
 package openmaple.data.pkgnx;
 
+import openmaple.data.DataEntry;
+import openmaple.data.DataSource;
+import openmaple.data.InvalidDataSourceException;
+import us.aaronweiss.pkgnx.NXFile;
+import us.aaronweiss.pkgnx.format.NXNode;
+
+import java.io.IOException;
+
 /**
- * @author Aaron
- * @version 1.0
+ * A {@code DataSource} for an {@code NXFile}.
+ *
+ * @author Aaron Weiss
+ * @version 1.0.0
  * @since 7/26/13
  */
-public class NXDataSource {
+public class NXDataSource extends DataSource {
+	private final NXFile source;
+	private DataEntry root;
+
+	/**
+	 * Creates an {@code NXFile} data source.
+	 *
+	 * @param filePath the path to the file
+	 * @throws InvalidDataSourceException if the file fails to load
+	 */
+	public NXDataSource(String filePath) {
+		try {
+			source = new NXFile(filePath);
+		} catch (IOException e) {
+			throw new InvalidDataSourceException("Failed to load file: " + filePath, e);
+		}
+	}
+
+	/**
+	 * Creates an {@code NXFile} data source.
+	 *
+	 * @param filePath         the path to the file
+	 * @param parseImmediately whether or not to parse it immediately
+	 * @throws InvalidDataSourceException if the file fails to load
+	 */
+	public NXDataSource(String filePath, boolean parseImmediately) {
+		try {
+			source = new NXFile(filePath, parseImmediately);
+		} catch (IOException e) {
+			throw new InvalidDataSourceException("Failed to load file: " + filePath, e);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return source.getFilePath();
+	}
+
+	@Override
+	public DataEntry getRoot() {
+		if (root == null) {
+			root = new NXDataEntry(source.getRoot(), "/");
+		}
+		return root;
+	}
+
+	@Override
+	protected void internalPopulateEntryMap() {
+		recursePopulate(source.getRoot(), "");
+	}
+
+	/**
+	 * Adds the specific node to the entry map.
+	 *
+	 * @param node      the node to add
+	 * @param pathAccum the path above this node
+	 */
+	private void recursePopulate(NXNode node, String pathAccum) {
+		pathAccum += "/" + node.getName();
+		dataEntries.put(pathAccum, new NXDataEntry(node, pathAccum));
+		for (NXNode child : node) {
+			recursePopulate(child, pathAccum);
+		}
+	}
+
+	@Override
+	protected DataEntry internalLookUp(String path) {
+		String pathProper = (path.startsWith("//")) ? path : (path.startsWith("/")) ? "/" + path : "//" + path;
+		return new NXDataEntry(source.resolve(path), pathProper);
+	}
 }
