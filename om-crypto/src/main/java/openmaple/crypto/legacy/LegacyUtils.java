fecee632f5fc378f5035082137acdf2e40c775a0
Aaron Weiss <aaronweiss74@gmail.com>
2013-08-19 02:55:02 -0400

    Updated javadoc comments for LegacyUtils.

--- om-crypto/src/main/java/openmaple/crypto/legacy/LegacyUtils.java
+++ om-crypto/src/main/java/openmaple/crypto/legacy/LegacyUtils.java
@@ -8,11 +8,25 @@ package openmaple.crypto.legacy;
  * @since 8/19/13
  */
 public class LegacyUtils {
+	/**
+	 * Rolls a byte to the left by the specific amount.
+	 *
+	 * @param b the byte to be rolled
+	 * @param c the amount to roll by
+	 * @return the rolled byte
+	 */
 	public static byte rollLeft(byte b, int c) {
 		int tmp = (b & 0xFF) << (c % 8);
 		return (byte) ((tmp & 0xFF) | (tmp >> 8));
 	}
 
+	/**
+	 * Rolls a byte to the right by the specific amount.
+	 *
+	 * @param b the byte to be rolled
+	 * @param c the amount to roll by
+	 * @return the rolled byte
+	 */
 	public static byte rollRight(byte b, int c) {
 		int tmp = ((b & 0xFF) << 8) >>> (c % 8);
 		return (byte) ((tmp & 0xFF) | (tmp >> 8));
