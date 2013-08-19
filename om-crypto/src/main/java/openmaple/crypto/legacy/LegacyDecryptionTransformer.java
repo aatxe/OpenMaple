5347ed3ce57b94fdafa42a0d6c36f04f55202bd8
Aaron Weiss <aaronweiss74@gmail.com>
2013-08-19 02:51:10 -0400

    Implemented unidirectional legacy transformers.

--- om-crypto/src/main/java/openmaple/crypto/legacy/LegacyDecryptionTransformer.java
+++ om-crypto/src/main/java/openmaple/crypto/legacy/LegacyDecryptionTransformer.java
@@ -1,9 +1,56 @@
 package openmaple.crypto.legacy;
 
+import io.netty.buffer.ByteBuf;
+import openmaple.crypto.ByteTransformer;
+
+import static openmaple.crypto.legacy.LegacyUtils.*;
+
 /**
- * @author Aaron
- * @version 1.0
+ * A unidirectional legacy transformer for the decryption end of the arbitrary MapleStory protocol.
+ *
+ * @author Aaron Weiss
+ * @version 1.0.0
  * @since 8/19/13
  */
-public class LegacyDecryptionTransformer {
+public class LegacyDecryptionTransformer implements ByteTransformer {
+	@Override
+	public void transform(byte[] data) {
+		byte cursor, prior;
+		for (int i = 0; i < 3; i++) {
+			prior = 0;
+			for (int j = data.length; j > 0; j--) {
+				cursor = data[j - 1];
+				cursor = (byte) (rollLeft(cursor, 3) ^ 0x13);
+				byte tmp = cursor;
+				cursor = (byte) ((cursor ^ prior) - j);
+				cursor = rollRight(cursor, 4);
+				prior = tmp;
+				data[j - 1] = cursor;
+			}
+			prior = 0;
+			for (int j = data.length; j > 0; j--) {
+				cursor = data[data.length - j];
+				cursor = (byte) (~(cursor - 0x48));
+				cursor = rollLeft(cursor, j);
+				byte tmp = cursor;
+				cursor = (byte) ((cursor ^ prior) - j);
+				cursor = rollRight(cursor, 3);
+				prior = tmp;
+				data[data.length - j] = cursor;
+			}
+		}
+	}
+
+	@Override
+	public void transform(ByteBuf data) {
+		data.markReaderIndex();
+		data.markWriterIndex();
+		data.clear();
+		byte[] trans = new byte[data.readableBytes()];
+		data.readBytes(trans);
+		transform(trans);
+		data.setBytes(0, trans);
+		data.resetReaderIndex();
+		data.resetWriterIndex();
+	}
 }
--- om-crypto/src/main/java/openmaple/crypto/legacy/LegacyEncryptionTransformer.java
+++ om-crypto/src/main/java/openmaple/crypto/legacy/LegacyEncryptionTransformer.java
@@ -1,11 +1,55 @@
 package openmaple.crypto.legacy;
 
+import io.netty.buffer.ByteBuf;
+import openmaple.crypto.ByteTransformer;
+
+import static openmaple.crypto.legacy.LegacyUtils.*;
+
 /**
- * A unidirectional legacy transformer for the arbitrary MapleStory protocol.
+ * A unidirectional legacy transformer for the encryption end of the arbitrary MapleStory protocol.
  *
  * @author Aaron Weiss
  * @version 1.0.0
  * @since 8/19/13
  */
-public class LegacyEncryptionTransformer {
+public class LegacyEncryptionTransformer implements ByteTransformer {
+	@Override
+	public void transform(byte[] data) {
+		byte cursor, prior;
+		for (int i = 0; i < 3; i++) {
+			prior = 0;
+			for (int j = data.length; j > 0; j--) {
+				cursor = data[data.length - j];
+				cursor = rollLeft(cursor, 3);
+				cursor = (byte) ((cursor + j) ^ prior);
+				prior = cursor;
+				cursor = rollRight(cursor, j);
+				cursor = (byte) (~cursor + 0x48);
+				data[data.length - j] = cursor;
+			}
+			prior = 0;
+			for (int j = data.length; j > 0; j--) {
+				cursor = data[j - 1];
+				cursor = rollLeft(cursor, 4);
+				cursor = (byte) ((cursor + j) ^ prior);
+				prior = cursor;
+				cursor ^= 0x13;
+				cursor = rollRight(cursor, j);
+				data[j - 1] = cursor;
+			}
+		}
+	}
+
+	@Override
+	public void transform(ByteBuf data) {
+		data.markReaderIndex();
+		data.markWriterIndex();
+		data.clear();
+		byte[] trans = new byte[data.readableBytes()];
+		data.readBytes(trans);
+		transform(trans);
+		data.setBytes(0, trans);
+		data.resetReaderIndex();
+		data.resetWriterIndex();
+	}
 }
