e41638cb81ae99dc12fbddac0e7c94993dc68956
Aaron Weiss <aaronweiss74@gmail.com>
2013-08-19 01:55:30 -0400

    Updated javadoc information for LBT.

--- om-crypto/src/main/java/openmaple/crypto/legacy/LegacyBaseTransformer.java
+++ om-crypto/src/main/java/openmaple/crypto/legacy/LegacyBaseTransformer.java
@@ -1,9 +1,13 @@
 package openmaple.crypto.legacy;
 
 /**
- * @author Aaron
- * @version 1.0
+ * The base transformer for the legacy MapleStory protocol.
+ *
+ * @author Aaron Weiss
+ * @version 1.0.0
  * @since 8/19/13
  */
 public class LegacyBaseTransformer {
+
+
 }
