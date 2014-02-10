399df77e5e8f2dad3c5929959367b93463164da3
Aaron Weiss <aaronweiss74@gmail.com>
2014-02-10 01:46:49 -0500

    Simplified LegacyFrameDecoder as a ReplayingDecoder.

--- om-network/src/main/java/openmaple/net/common/LegacyFrameDecoder.java
+++ om-network/src/main/java/openmaple/net/common/LegacyFrameDecoder.java
@@ -2,7 +2,7 @@ package openmaple.net.common;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.handler.codec.ReplayingDecoder;
 import openmaple.crypto.legacy.LegacyCryptoKit;
 import openmaple.net.common.utils.NetworkUtils;
 
@@ -15,20 +15,16 @@ import java.util.List;
  * @version 1.0.0
  * @since 1/26/14
  */
-public class LegacyFrameDecoder extends ByteToMessageDecoder {
+public class LegacyFrameDecoder extends ReplayingDecoder<ByteBuf> {
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		LegacyCryptoKit cryptoKit = (LegacyCryptoKit) ctx.attr(NetworkUtils.CRYPTO_KIT_ATTRIBUTE_KEY).get();
-		if (in.readableBytes() < 4)
-			return;
 		int length = in.readInt();
 		if (!cryptoKit.validatePacket(length)) {
 			ctx.close();
 			return;
 		}
 		length = cryptoKit.getPacketLength(length);
-		if (in.readableBytes() < length)
-			return;
 		ByteBuf data = in.readBytes(length);
 		cryptoKit.decrypt(data);
 		out.add(data);
