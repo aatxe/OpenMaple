19f4eb3abd18d5d15890ed24bec5ff5bdc6d74be
Aaron Weiss <aaronweiss74@gmail.com>
2014-02-10 21:51:48 -0500

    Implemented reflection-based Packet serialization.

--- om-network/src/main/java/openmaple/net/common/PacketSerializer.java
+++ om-network/src/main/java/openmaple/net/common/PacketSerializer.java
@@ -1,9 +1,102 @@
 package openmaple.net.common;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import openmaple.net.common.annotate.Opcode;
+import openmaple.net.common.annotate.Serial;
+import openmaple.net.common.serial.BaseSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Aaron
  * @version 1.0
  * @since 2/10/14
  */
-public class PacketSerializer {
+public class PacketSerializer extends MessageToByteEncoder {
+	private final static Logger logger = LoggerFactory.getLogger(PacketSerializer.class);
+	private Map<Class<?>, Constructor<?>> classConstructorMap = new HashMap<>();
+
+	private Constructor<?> classToConstructor(Class<?> c) {
+		if (classConstructorMap.containsKey(c))
+			return classConstructorMap.get(c);
+		for (Constructor<?> constructor : c.getConstructors()) {
+			if (constructor.isAnnotationPresent(Opcode.class)) {
+				classConstructorMap.put(c, constructor);
+				return constructor;
+			}
+		}
+		throw new Error("OpenMaple was unable to find a constructor for the class " + c.getSimpleName());
+	}
+
+	private int classToOpcodeValue(Class<?> c) {
+		return classToConstructor(c).getAnnotation(Opcode.class).value();
+	}
+
+	private SerializeData[] fieldValues(Object o) {
+		Field[] fields = o.getClass().getFields();
+		SerializeData[] serializeData = new SerializeData[fields.length];
+		int index = 0;
+		for (Field field : fields)
+			if (field.isAnnotationPresent(Serial.class))
+				serializeData[index++] = new SerializeData(o, field);
+		Arrays.sort(serializeData);
+		return serializeData;
+	}
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
+		int opcode = classToOpcodeValue(msg.getClass());
+		SerializeData[] data = fieldValues(msg);
+		out.writeShort((short) opcode);
+		for (SerializeData d : data) {
+			out.writeBytes(BaseSerializer.serialize(d.get(), d.type(), d.annotations()));
+		}
+	}
+
+	private static class SerializeData implements Comparable<SerializeData> {
+		private final Object msg;
+		private final Field field;
+		private final int position;
+
+		public SerializeData(Object msg, Field field) {
+			this(msg, field, field.getAnnotation(Serial.class).value());
+		}
+
+		public SerializeData(Object msg, Field field, int position) {
+			this.msg = msg;
+			this.field = field;
+			this.position = position;
+		}
+
+		public Object get() {
+			try {
+				return field.get(msg);
+			} catch (IllegalAccessException e) {
+				logger.error("", e);
+			}
+			return null;
+		}
+
+		public Class<?> type() {
+			return field.getType();
+		}
+
+		public Annotation[] annotations() {
+			return field.getAnnotations();
+		}
+
+		@Override
+		public int compareTo(SerializeData o) {
+			return position - o.position;
+		}
+	}
 }
--- om-network/src/main/java/openmaple/net/common/serial/BaseSerializer.java
+++ om-network/src/main/java/openmaple/net/common/serial/BaseSerializer.java
@@ -1,9 +1,39 @@
 package openmaple.net.common.serial;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import openmaple.net.common.annotate.Unsigned;
+
+import java.lang.annotation.Annotation;
+
 /**
  * @author Aaron
  * @version 1.0
  * @since 2/10/14
  */
 public class BaseSerializer {
+	public static ByteBuf serialize(Object o, Class<?> type, Annotation[] annotations) {
+		boolean unsigned = false;
+		for (Annotation annotation : annotations)
+			if (annotation.getClass().equals(Unsigned.class))
+				unsigned = true;
+		if (type.equals(int.class)) {
+			return (unsigned) ? Unpooled.buffer(2).writeShort((short) o) : Unpooled.buffer(4).writeInt((int) o);
+		} else if (type.equals(short.class)) {
+			return (unsigned) ? Unpooled.buffer(1).writeByte((byte) o) : Unpooled.buffer(2).writeShort((short) o);
+		} else if (type.equals(long.class)) {
+			return (unsigned) ? Unpooled.buffer(4).writeInt((int) o) : Unpooled.buffer(8).writeLong((long) o);
+		} else if (type.equals(byte.class)) {
+			return Unpooled.buffer(1).writeByte((byte) o);
+		} else if (type.equals(double.class)) {
+			return Unpooled.buffer(8).writeDouble((double) o);
+		} else if (type.equals(float.class)) {
+			return Unpooled.buffer(4).writeFloat((float) o);
+		} else if (type.equals(String.class)) {
+			// TODO: serialize string;
+			return null;
+		} else {
+			throw new Error("OpenMaple found an unknown base type (" + type.getSimpleName() + ") in the serialization process.");
+		}
+	}
 }
