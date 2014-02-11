e769d9f04e9c9edc21294192783adb757d756fa5
Aaron Weiss <aaronweiss74@gmail.com>
2014-02-10 21:00:56 -0500

    Added Serial annotation for field serialization.

--- om-network/src/main/java/openmaple/net/common/annotate/Serial.java
+++ om-network/src/main/java/openmaple/net/common/annotate/Serial.java
@@ -1,9 +1,19 @@
 package openmaple.net.common.annotate;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
- * @author Aaron
- * @version 1.0
+ * A field annotation that marks the order for the fields to be serialized in.
+ *
+ * @author Aaron Weiss
+ * @version 1.0.0
  * @since 2/10/14
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Serial {
+	public int value();
 }
